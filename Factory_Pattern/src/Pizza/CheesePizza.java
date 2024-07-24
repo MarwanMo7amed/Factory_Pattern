684bca6f2139dec0ca54a78eb5ac81c6413c267f
mmohamed <Nour@2000->
2024-07-24 09:50:05 +0300

    changes in cheesePizze

--- Factory_Pattern/src/Pizza/CheesePizza.java
+++ Factory_Pattern/src/Pizza/CheesePizza.java
@@ -2,7 +2,7 @@ package Pizza;
 
 public class CheesePizza extends Pizza{
     public CheesePizza() {
-        name = "Cheese Pizza";
+        name = "Cheese Pizza  -----";
         dough = "Regular Crust";
         sauce = "Marinara Pizza Sauce";
         toppings.add("Fresh Mozzarella");
